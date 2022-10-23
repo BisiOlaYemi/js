a30872b9268660c8ed4b60237dc25e3662cead7b
OLAYEMI OLUBISI <46648435+BisiOlaYemi@users.noreply.github.com>
2022-10-23 15:43:46 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let myAge = 22
+let sum = 200
+let totalCount = 5
+
+function addsToMyAge(sustractFrom, multiplyBy) {
+  
+  return sum = sum + 100
+  totalCount = totalCount + 200
+ 
+  
+}
+
+
+console.log(sum, totalCount)
+
+
+
+
+
+
+
+
